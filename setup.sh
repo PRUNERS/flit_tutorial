673f457d894ee586a8557fb6c70840447441ca99
Michael Bentley <mikebentley15@gmail.com>
2019-07-01 17:30:13 -0600

    Downgrade MFEM version to 3.4 from 4.0
    This is because the 4.0 version doesn't have very high variability
    for example 13.

--- setup.sh
+++ setup.sh
@@ -7,28 +7,31 @@ mkdir -p packages
 pushd packages
 
 export MAKEFLAGS=-j8
+export HYPRE_VERSION=2.11.2
+export METIS_VERSION=4.0.3
+export MFEM_VERSION=3.4
+# export GLVIS_VERSION=3.4
 
 # clone and build hypre
-export HYPRE_VERSION=2.16.0
 wget https://github.com/hypre-space/hypre/archive/v${HYPRE_VERSION}.tar.gz
 tar -xzf v${HYPRE_VERSION}.tar.gz
 rm v${HYPRE_VERSION}.tar.gz
 ln -s hypre-${HYPRE_VERSION} hypre
+ln -s hypre-${HYPRE_VERSION} hypre-2.10.0b
 pushd hypre/src
 ./configure --disable-fortran
 make
 popd
 
 # clone and build metis
-export METIS_VERSION=4.0.3
 wget http://glaros.dtc.umn.edu/gkhome/fetch/sw/metis/OLD/metis-${METIS_VERSION}.tar.gz
 tar -xzf metis-${METIS_VERSION}.tar.gz
 make --directory metis-${METIS_VERSION}
+ln -s metis-${METIS_VERSION} metis
 ln -s metis-${METIS_VERSION} metis-4.0
 rm metis-${METIS_VERSION}.tar.gz
 
 # clone and build mfem
-export MFEM_VERSION=4.0
 wget https://github.com/mfem/mfem/archive/v${MFEM_VERSION}.tar.gz
 tar -xzf v${MFEM_VERSION}.tar.gz
 rm v${MFEM_VERSION}.tar.gz
@@ -37,7 +40,6 @@ make config MFEM_USE_MPI=YES --directory mfem-${MFEM_VERSION}
 make --directory mfem-${MFEM_VERSION}
 
 ## # clone and build glvis
-## export GLVIS_VERSION=3.4
 ## wget http://glvis.github.io/releases/glvis-${GLVIS_VERSION}.tgz
 ## tar -xzf glvis-${GLVIS_VERSION}.tgz
 ## rm glvis-${GLVIS_VERSION}.tgz
