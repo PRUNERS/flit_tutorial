0d1d7f542f0f6bdd8dff08c4562a58dcf60f88fb
Michael Bentley <mikebentley15@gmail.com>
2019-07-08 18:19:57 -0600

    Make LULESH example 3

--- example_01/custom.mk
+++ example_01/custom.mk
@@ -1,20 +1,30 @@
-LULESH_DIR     := ../packages/LULESH
+# This file is included by the generated Makefile.  If you have some things you
+# want to change about the Makefile, it is best to do it here.
 
+PACKAGES_DIR   := $(abspath ../packages)
+MFEM_SRC       := $(PACKAGES_DIR)/mfem
+HYPRE_SRC      := $(PACKAGES_DIR)/hypre
+METIS_SRC      := $(PACKAGES_DIR)/metis-4.0
+
+SOURCE         :=
 SOURCE         += $(wildcard *.cpp)
 SOURCE         += $(wildcard tests/*.cpp)
-SOURCE         += $(wildcard $(LULESH_DIR)/*.cc)
 
-# Remove lulesh.cc since it defines main().  It is included from LuleshTest.cpp
-SOURCE         := $(filter-out $(LULESH_DIR)/lulesh.cc,$(SOURCE))
+# Compiling all sources of MFEM into the tests takes too long for a tutorial
+# skip it.  Instead, we link in the MFEM static library
+#SOURCE         += $(wildcard ${MFEM_SRC}/fem/*.cpp)
+#SOURCE         += $(wildcard ${MFEM_SRC}/general/*.cpp)
+#SOURCE         += $(wildcard ${MFEM_SRC}/linalg/*.cpp)
+#SOURCE         += $(wildcard ${MFEM_SRC}/mesh/*.cpp)
 
+# just the one source file to see there is a difference
+SOURCE         += ${MFEM_SRC}/linalg/densemat.cpp  # where the bug is
 
-CC_REQUIRED    += -I$(LULESH_DIR)
-CC_REQUIRED    += -DUSE_MPI=1
-CC_REQUIRED    += -Wno-unknown-pragmas
-CC_REQUIRED    += -Wno-unused-parameter
+CC_REQUIRED    += -I${MFEM_SRC}
+CC_REQUIRED    += -I${MFEM_SRC}/examples
+CC_REQUIRED    += -isystem ${HYPRE_SRC}/src/hypre/include
 
-LD_REQUIRED    += -lm
+LD_REQUIRED    += -L${MFEM_SRC} -lmfem
+LD_REQUIRED    += -L${HYPRE_SRC}/src/hypre/lib -lHYPRE
+LD_REQUIRED    += -L${METIS_SRC} -lmetis
 
-.PHONY: print-%
-print-%:
-	@echo '$*=$($*)'
