8e9cb63643343413db5312ed3bd89cc4d8596db0
Michael Bentley <mikebentley15@gmail.com>
2019-07-17 17:14:18 -0600

    step-10: use pygmentize

--- exercise-2/step-10.sh
+++ exercise-2/step-10.sh
@@ -1,4 +1,5 @@
 #!/bin/bash -x
-cat -n ../packages/mfem/linalg/densemat.cpp 2>/dev/null | \
+pygmentize ../packages/mfem/linalg/densemat.cpp 2>/dev/null | \
+  cat -n | \
   tail -n +3680 | \
   head -n 40
