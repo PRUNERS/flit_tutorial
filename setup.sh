1ed808b15316f7e2a234c4266d39d1d92f1ddb99
Michael Bentley <mikebentley15@gmail.com>
2019-11-11 10:11:42 -0700

    setup.sh: use alternative for metis - orig site is down

--- setup.sh
+++ setup.sh
@@ -24,12 +24,22 @@ make
 popd
 
 # clone and build metis
-wget http://glaros.dtc.umn.edu/gkhome/fetch/sw/metis/OLD/metis-${METIS_VERSION}.tar.gz
-tar -xzf metis-${METIS_VERSION}.tar.gz
-make --directory metis-${METIS_VERSION}
+## seems glaros.dtc.umn.edu is down, alternative used
+#wget http://glaros.dtc.umn.edu/gkhome/fetch/sw/metis/OLD/metis-${METIS_VERSION}.tar.gz
+#tar -xzf metis-${METIS_VERSION}.tar.gz
+#make --directory metis-${METIS_VERSION}
+#ln -s metis-${METIS_VERSION} metis
+#ln -s metis-${METIS_VERSION} metis-4.0
+#rm metis-${METIS_VERSION}.tar.gz
+
+# clone and build metis using someone's clone
+git clone https://github.com/CIBC-Internal/metis-${METIS_VERSION}.git
+cd metis-${METIS_VERSION}
+cmake .
+make
+cd ..
 ln -s metis-${METIS_VERSION} metis
 ln -s metis-${METIS_VERSION} metis-4.0
-rm metis-${METIS_VERSION}.tar.gz
 
 # clone and build mfem
 wget https://github.com/mfem/mfem/archive/v${MFEM_VERSION}.tar.gz
